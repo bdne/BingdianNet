24e85bf0d5f20fb09f6d14c3afa8bca7001c2692
bdne <lishichao@bupt.edu.cn>
2021-10-24 22:19:43 +0800

    10.24晚，完成UNet，在做UNet++

--- train.py
+++ train.py
@@ -61,9 +61,9 @@ class Segmentation(object):
         ])
 
         #存储图片id，区分训练集与验证集
-        ori_img_path = "D:\\python\\BingdianNet\\raw\\train"
+        ori_img_path = "D:\\python\\BingdianNet\\raw\\unet\\train"
         ori_list = sorted(os.listdir(ori_img_path))
-        label_path = "D:\\python\\BingdianNet\\raw\\label"
+        label_path = "D:\\python\\BingdianNet\\raw\\unet\\label"
         label_list = sorted(os.listdir(label_path))
         train_img_list, val_img_list = ori_list[:24], ori_list[24:]
         train_label_list, val_label_list = label_list[:24], label_list[24:]
@@ -133,7 +133,7 @@ class Segmentation(object):
         print('\nTest set: Batch average loss: {:.4f}, Dice Coefficient: {:.2f}%\n'.format(test_loss, dice_acc))
 
         if dice_acc > self.best_dice:
-            torch.save(self.model.state_dict(), self.save_folder + self.save_model_name)
+            torch.save(self.model.state_dict(), self.save_folder + 'UNet\\'+self.save_model_name)
             self.best_dice = dice_acc
             print("======Saving model======")
     def main(self):
@@ -141,7 +141,133 @@ class Segmentation(object):
             Segmentation.train(self,epoch)
             Segmentation.test(self)
 
+
+
+from Net.UNetplus import *
+import albumentations as A
+from albumentations.core.composition import Compose, OneOf
+from Utils.Dataset import UNetplusDataset
+from sklearn.model_selection import train_test_split
+from Utils.scripts import AverageMeter,str2bool,count_params
+class seg_UNetplus(object):
+    def init(self):     
+
+        self.batch_size=2
+        self.test_batch_size=2
+        self.epochs=1
+        self.lr=0.01
+        self.cuda=True
+        self.log_interval=3
+        self.num_classes=1
+        self.deep_supervision=False
+        self.input_channels=1
+        self.save_folder='D:\\python\\BingdianNet\\result\\unet++'
+        self.save_model_name='my_unet++.pth'
+        self.input_h=96
+        self.input_w=96
+
+        #数据存储地址
+        img_root='D:\\python\\pycharm\\unet\\Unet-pytorch\\raw\\train'
+        img_list= sorted(os.listdir(img_root))
+        label_root='D:\\python\\pycharm\\unet\\Unet-pytorch\\raw\\train'
+        label_list= sorted(os.listdir(label_root))
+        train_img_list, val_img_list = train_test_split(img_list, test_size=0.2, random_state=41)
+        #训练数据增强img_rootlabel_list
+        train_transform = Compose([
+                A.RandomRotate90(),
+                transforms.Flip(),
+                OneOf([
+                    transforms.HueSaturationValue(),
+                    transforms.RandomBrightness(),
+                    transforms.RandomContrast(),
+                ], p=1),
+                A.Resize(self.input_h, self.input_w),
+                transforms.Normalize(),
+            ])
+
+        val_transform = Compose([
+            A.Resize(self.input_h, self.input_w),
+            transforms.Normalize(),
+        ])
+        train_dataset = UNetplusDataset(
+            img_root=img_root,
+            label_root=label_root,
+            img_list=train_img_list,
+            #label_list=label_list,
+            transform=train_transform,
+            target_transform = val_transform
+            )
+        val_dataset = UNetplusDataset(
+            img_root=img_root,
+            label_root=label_root,
+            img_list=val_img_list,
+            #label_list=label_list,
+            transform=train_transform,
+            target_transform = val_transform
+            )
+        kwargs = {'num_workers': 0, 'pin_memory': False} if self.cuda else {}
+        train_loader = torch.utils.data.DataLoader(
+                    train_dataset,
+                    batch_size=self.batch_size,
+                    shuffle=True,
+                    drop_last=True,**kwargs)
+        val_loader = torch.utils.data.DataLoader(
+                    val_dataset,
+                    batch_size=self.batch_size,
+                    shuffle=False,
+                    drop_last=False,**kwargs)
+
+        if not os.path.isdir(self.save_folder):
+            os.mkdir(self.save_folder)
+
+        self.model = NestedUNet(self.num_classes,self.input_channels,self.deep_supervision)#第一项参数为num_classes，在本数据集中为1
+
+        self.model = self.model.cuda()
         
-seg_UNet=Segmentation()
-seg_UNet.main()
+        def train(config, train_loader, model, criterion, optimizer):
+            avg_meters = {'loss': AverageMeter(),
+                        'iou': AverageMeter()}
+            model.train()
+
+            pbar = tqdm(total=len(train_loader))
+            for input, target, _ in train_loader:
+                input = input.cuda()
+                target = target.cuda()
+
+                # compute output
+                if config['deep_supervision']:
+                    outputs = model(input)
+                    loss = 0
+                    for output in outputs:
+                        loss += criterion(output, target)
+                    loss /= len(outputs)
+                    iou = iou_score(outputs[-1], target)
+                else:
+                    output = model(input)
+                    loss = criterion(output, target)
+                    iou = iou_score(output, target)
+
+                # compute gradient and do optimizing step
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                avg_meters['loss'].update(loss.item(), input.size(0))
+                avg_meters['iou'].update(iou, input.size(0))
+
+                postfix = OrderedDict([
+                    ('loss', avg_meters['loss'].avg),
+                    ('iou', avg_meters['iou'].avg),
+                ])
+                pbar.set_postfix(postfix)
+                pbar.update(1)
+            pbar.close()
+
+            return OrderedDict([('loss', avg_meters['loss'].avg),
+                                ('iou', avg_meters['iou'].avg)])
+
+
+         
+# seg_UNet=Segmentation()
+# seg_UNet.main()
 
